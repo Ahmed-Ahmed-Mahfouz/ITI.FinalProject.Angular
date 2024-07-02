1038c4dd5c7bdf8afa5a8801fdf375c3e0d14dd2
abeerzahran <abeerzahran32@gmail.com>
2024-07-02 23:56:33 +0300

    adding component folder in Admin , adding representative and branch DTOs and services

---
+++ src/admin/Services/representative.service.ts
@@ -0,0 +1,29 @@
+import { AddRepresentative } from '../DTOs/InsertDTOs/addRepresentative';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepresentativeService {
+
+  baseURL:string="https://localhost:7057/api/Representative/";
+  constructor(public httpClient:HttpClient) { }
+
+  addRepresentative(representative:AddRepresentative){
+    return this.httpClient.post(this.baseURL,representative);
+  }
+  getAllRepresentatives(){
+    return this.httpClient.get(this.baseURL);
+  }
+  getRepresentative(id:string){
+    return this.httpClient.get(this.baseURL+id);
+  }
+  editRepresentative(id:string,representative:AddRepresentative){
+    return this.httpClient.put(this.baseURL+id,representative);
+  }
+  deleteRepresentative(id:string){
+    return this.httpClient.delete(this.baseURL+id);
+  }
+
+}
