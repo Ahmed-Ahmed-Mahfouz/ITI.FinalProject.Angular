620b44214166f319932831367f494ef3e01e7e95
SaadHany99 <159723036+SaadHany99@users.noreply.github.com>
2024-07-04 12:59:13 +0300

    Revert "Merge branch 'Ahmed' into Saad"
    This reverts commit e2e20c30d744b7ed45cd0a3677ee1ed3e256e050, reversing
    changes made to a557c2bbdccc7e780e43dc15d121db8e5a583556.

--- src/app/shared-module/Components/order-report/order-report.component.ts
+++ src/app/shared-module/Components/order-report/order-report.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-order-report',
   standalone: true,
   imports: [],
   templateUrl: './order-report.component.html',
-  styleUrl: './order-report.component.css',
+  styleUrl: './order-report.component.css'
 })
-export class OrderReportComponent {}
+export class OrderReportComponent {
+
+}
