5fa78e020825b24efb05d5db747b0e3693416016
SaadHany99 <159723036+SaadHany99@users.noreply.github.com>
2024-07-06 12:54:05 +0300

    Add Settings into Employee Module

--- src/employee/employee-routing.module.ts
+++ src/employee/employee-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SettingComponent } from './settings/setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'settings', component:SettingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
