d751365b037b3bde21be9058fe521656d4e75470
Ahmed Mahfouz <ahmed.ahmed240@edu.menofia.edu.eg>
2024-07-07 10:06:37 +0300

    test 2

--- src/admin/admin-routing.module.ts
+++ src/admin/admin-routing.module.ts
@@ -1,21 +1,25 @@
 import { RepresentativeListComponent } from './Components/representative-list/representative-list.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MerchantAddComponent } from './Components/merchant-add/merchant-add.component';
 import { RepresentativeFormComponent } from './Components/representative-form/representative-form.component';
 import { BranchFromComponent } from './Components/branch-from/branch-from.component';
 import { GovernorateListComponent } from './Components/governorate-list/governorate-list.component';
 import { BranchListComponent } from './Components/branch-list/branch-list.component';
+import { MerchantListComponent } from './Components/merchant-list/merchant-list.component';
+import { MerchantEditComponent } from './Components/merchant-edit/merchant-edit.component';
 
 const routes: Routes = [
-  {path:'representative',component:RepresentativeListComponent},
+  { path: 'representative', component: RepresentativeListComponent },
   { path: 'representative/add', component: RepresentativeFormComponent },
   { path: 'representative/edit/:id', component: RepresentativeFormComponent },
+  { path: 'merchant', component: MerchantListComponent },
   { path: 'merchant/add', component: MerchantAddComponent },
-  {path:"branch",component:BranchListComponent},
+  { path: 'merchant/edit/id', component: MerchantEditComponent },
+  { path: 'branch', component: BranchListComponent },
   { path: 'branch/add', component: BranchFromComponent },
   { path: 'branch/edit/:id', component: BranchFromComponent },
-  { path: 'governorate', component:GovernorateListComponent}
+  { path: 'governorate', component: GovernorateListComponent },
 ];
 
 @NgModule({
