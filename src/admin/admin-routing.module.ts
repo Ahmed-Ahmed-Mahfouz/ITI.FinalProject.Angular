0132951de4038fc6d47f5d21a80bf71a73b83430
abeerzahran <abeerzahran32@gmail.com>
2024-07-07 10:16:26 +0300

    merchant list , edit

--- src/admin/admin-routing.module.ts
+++ src/admin/admin-routing.module.ts
@@ -6,12 +6,16 @@ import { RepresentativeFormComponent } from './Components/representative-form/re
 import { BranchFromComponent } from './Components/branch-from/branch-from.component';
 import { GovernorateListComponent } from './Components/governorate-list/governorate-list.component';
 import { BranchListComponent } from './Components/branch-list/branch-list.component';
+import { MerchantListComponent } from './Components/merchant-list/merchant-list.component';
+import { MerchantEditComponent } from './Components/merchant-edit/merchant-edit.component';
 
 const routes: Routes = [
   {path:'representative',component:RepresentativeListComponent},
   { path: 'representative/add', component: RepresentativeFormComponent },
   { path: 'representative/edit/:id', component: RepresentativeFormComponent },
   { path: 'merchant/add', component: MerchantAddComponent },
+  { path: 'merchant/edit/:id', component: MerchantEditComponent },
+  { path: 'merchant', component: MerchantListComponent },
   {path:"branch",component:BranchListComponent},
   { path: 'branch/add', component: BranchFromComponent },
   { path: 'branch/edit/:id', component: BranchFromComponent },
